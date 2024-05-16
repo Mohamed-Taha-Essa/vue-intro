1201d12f19659d259b27ea9dc854406776b179b5
Mohamed Abdallah Essa <www.taha.166@gmail.com>
2024-05-17 02:21:21 +0300

    add form

--- main.js
+++ main.js
@@ -1,47 +1,28 @@
 const app = Vue.createApp({
     data() {
-        return({
-            cart: 0,
-            product: 'Socks',
-            brand: 'Vue Mastery',
-            desc : 'hello from view',
-            selectedVariant: 0,
-            details: ['50% cotton', '30% wool', '20% polyester'],
-            variants: [
-                { id: 2234, color: 'green',image:'./assets/images/socks_green.jpg' ,quantity :3},
-                { id: 2235, color: 'blue' ,image:'./assets/images/socks_blue.jpg',quantity :0}
-              ]
-        
-        })
+        return(
+            {
+            cart: [],
+            premium:true,
+            }
+        )
         
     },
 
     methods :{
-       
-        addToCart(){
-            this.cart +=1
-            this.variants[this.selectedVariant].quantity -=1
-        },
-        updateVariant(index) {
-            this.selectedVariant = index
-          
-        },
-       
+        updateCart(id){
+            this.cart.push(id)
+            console.log(this.cart.length)
 
+        },
+        moveProduct(){
+            if (this.cart.length > 0) {
+                this.cart.pop();
+              }  
+              console.log(this.cart.length)
+     
+             }
 
     },
-    computed:{
-        title(){
-            return  this.brand+' '+this.product
-        },
-
-        image(){
-            return this.variants[this.selectedVariant].image
-        },
-
-        instock(){
-            return this.variants[this.selectedVariant].quantity
-        },
-
-    }
+    computed:{}
 })
